b5eee2377e8476ea112af4b69ce5939c069719a8
Baptiste SAEGAERT <baptiste.saegaert@gmail.com>
2025-04-17 15:48:22 +0200

    Remplacer les chemins des ressources pour assurer leur accessibilité et définir des variables de couleur pour remplacer Radix UI

--- assets/js/dark.js
+++ assets/js/dark.js
@@ -1,19 +1,23 @@
 document.addEventListener('DOMContentLoaded', (event) => {
     const themeToggle = document.getElementById('themeToggle');
-    const currentTheme = localStorage.getItem('theme') || 'light';
+    
+    // Vérifier si l'élément existe avant d'ajouter un écouteur d'événement
+    if (themeToggle) {
+        const currentTheme = localStorage.getItem('theme') || 'light';
 
-    if (currentTheme === 'dark') {
-        document.body.classList.add('dark');
-        themeToggle.checked = true;
-    }
-
-    themeToggle.addEventListener('change', () => {
-        if (themeToggle.checked) {
+        if (currentTheme === 'dark') {
             document.body.classList.add('dark');
-            localStorage.setItem('theme', 'dark');
-        } else {
-            document.body.classList.remove('dark');
-            localStorage.setItem('theme', 'light');
+            themeToggle.checked = true;
         }
-    });
+
+        themeToggle.addEventListener('change', () => {
+            if (themeToggle.checked) {
+                document.body.classList.add('dark');
+                localStorage.setItem('theme', 'dark');
+            } else {
+                document.body.classList.remove('dark');
+                localStorage.setItem('theme', 'light');
+            }
+        });
+    }
 });
